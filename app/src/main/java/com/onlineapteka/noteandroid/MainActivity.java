370495facd2d0a019ee4555a53aa00cb4dc8b275
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-16 23:35:06 +0600

    refactoring fonts

--- app/src/main/java/com/onlineapteka/noteandroid/MainActivity.java
+++ app/src/main/java/com/onlineapteka/noteandroid/MainActivity.java
@@ -14,35 +14,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView recyclerView;
-    Button button;
-    MainAdapter adapter;
-    List<ItemModel> itemModelList;
-    EditText editText;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.main_recycler);
-        button = findViewById(R.id.button_add);
-        editText = findViewById(R.id.title_edit_text);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (TextUtils.isEmpty(editText.getText().toString())){
-
-                }else {
-
-                }
-            }
-        });
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new MainAdapter(this,itemModelList);
-        recyclerView.setAdapter(adapter);
-
     }
 }
