03c5065ab34b0d93f53098e122f3d13341061a9f
rustamkeneev <dr.rustamkeneev@gmail.com>
2021-04-17 01:49:01 +0600

    update main and create not activity

--- app/src/main/java/com/onlineapteka/noteandroid/CreateNoteActivity.java
+++ app/src/main/java/com/onlineapteka/noteandroid/CreateNoteActivity.java
@@ -3,12 +3,26 @@ package com.onlineapteka.noteandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CreateNoteActivity extends AppCompatActivity {
+    private ImageView imageBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+        initViews();
+    }
+
+    private void initViews() {
+        imageBack = findViewById(R.id.image_back);
+        imageBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
--- app/src/main/java/com/onlineapteka/noteandroid/MainActivity.java
+++ app/src/main/java/com/onlineapteka/noteandroid/MainActivity.java
@@ -4,19 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+
+    private ImageView imageAddMainNote;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initviews();
+    }
+
+    private void initviews() {
+        imageAddMainNote = findViewById(R.id.image_add_main_note);
+        imageAddMainNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(
+                        new Intent(getApplicationContext(),CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE
+                );
+            }
+        });
     }
 }
